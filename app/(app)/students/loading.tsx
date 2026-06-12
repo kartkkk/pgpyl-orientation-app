b1d3bf3dbf2c02d0091b3e3444b60fdea69a2c70
Karthik Mohan <108178127+kartkkk@users.noreply.github.com>
2026-06-12 12:41:08 +0530

    Delete app directory

--- app/(app)/students/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function StudentsLoading() {
-  return (
-    <div className="space-y-3 p-4">
-      {Array.from({ length: 6 }).map((_, i) => (
-        <div key={i} className="h-14 animate-pulse rounded-2xl bg-gray-100" />
-      ))}
-    </div>
-  );
-}
